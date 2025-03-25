344c746fc8d322c5b52b8b63b6a9ae0ad3b18235
Emagmunioz <emagmunioz@gmail.com>
2025-03-25 15:25:33 +0100

    Implement Computador Constructor (Green)

---
+++ src/main/java/computer/Computador.java
@@ -0,0 +1,12 @@
+package computer;
+
+public class Computador {
+    
+    private final String marca;
+    public Computador(String marca, int memoria, String procesador, String so, double precio) {
+        this.marca = marca;
+    }
+    public String getMarca() { return marca; }
+}
+
+
--- src/test/java/computer/ComputadorTest.java
+++ src/test/java/computer/ComputadorTest.java
@@ -1,13 +1,13 @@
 package computer;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
-
 public class ComputadorTest {
    
 @Test
 public void whenCreateComputador_thenPropertiesSetCorrectly() {
     Computador pc = new Computador("Dell", 16, "i7", "Windows", 1500.0);
-    assertThat(pc.getMarca(), is("Dell")); // Fails: Class doesn't exist
+    assertThat(pc.getMarca(), is("Dell")); 
 }
 
 }
